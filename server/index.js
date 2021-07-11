590043a0308e7a82200e3d002865823431ba06d7
Shikha <41201838+shikhasingh07@users.noreply.github.com>
2021-07-11 22:24:57 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRouter from "./router/posts.js";
+import dotenv from "dotenv";
+const app = express();
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: "30mb" }));
+app.use(cors());
+app.use("/posts", postRouter);
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("Welcome To Memories Api");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
