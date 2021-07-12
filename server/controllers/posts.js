debd97e450122e3c4ed88d997f90b96849039eba
Shikha <41201838+shikhasingh07@users.noreply.github.com>
2021-07-12 22:25:41 +0530

    Add files via upload

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,62 +1,77 @@
-import express from 'express';
-import PostMessage from '../model/postMessage.js';
-import  mongoose  from 'mongoose';
+import express from "express";
+import PostMessage from "../model/postMessage.js";
+import mongoose from "mongoose";
 const router = express.Router();
 
-export const getPosts = async (req, res) => { 
-    try {
-        const postMessages = await PostMessage.find();
-        res.status(200).json(postMessages);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 export const createPost = async (req, res) => {
-    const { title, message, selectedFile, creator, tags } = req.body;
+  const post = req.body;
 
-    const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
+  const newPostMessage = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
 
-    try {
-        await newPostMessage.save();
+  try {
+    await newPostMessage.save();
 
-        res.status(201).json(newPostMessage );
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
-}
+    res.status(201).json(newPostMessage);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
 
 export const updatePost = async (req, res) => {
-    const { id } = req.params;
-    const { title, message, creator, selectedFile, tags } = req.body;
-    
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  const { id } = req.params;
+  const { title, message, creator, selectedFile, tags } = req.body;
 
-    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
 
-    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+  const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
-    res.json(updatedPost);
-}
+  await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+  res.json(updatedPost);
+};
 
 export const deletePost = async (req, res) => {
-    const { id } = req.params;
-     
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+  const { id } = req.params;
 
-    await PostMessage.findByIdAndDelete(id);
-   
-    res.json({ message: "Post deleted successfully." });
-}
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  await PostMessage.findByIdAndDelete(id);
+
+  res.json({ message: "Post deleted successfully." });
+};
 
 export const likePost = async (req, res) => {
-    const { id } = req.params;
-     console.log('Like')
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
-    
-    const post = await PostMessage.findById(id);
+  const { id } = req.params;
+  console.log("Like");
+  if (!req.userId) return res.json({ message: "unauthorized" });
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
-     
-    res.json(updatedPost);
-}
+  const post = await PostMessage.findById(id);
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+  if (index === -1) {
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
+
+  res.json(updatedPost);
+};
 export default router;
--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRouter from "./router/posts.js";
+import userRoutes from "./router/users.js";
 import dotenv from "dotenv";
 const app = express();
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json({ limit: "30mb" }));
 app.use(cors());
 app.use("/posts", postRouter);
+app.use("/user", userRoutes);
 dotenv.config();
 
 app.get("/", (req, res) => {
