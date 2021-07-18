b791d167f629378b2c0b1ed2b1f44bcd2c899215
shikhasingh07 <shikha.thakur2295@gmail.com>
2021-07-18 22:25:05 +0530

    comiit on main

---
+++ client/src/Compoents/Pagination.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Pagination, PaginationItem } from '@material-ui/lab';
+import { Link } from 'react-router-dom';
+
+import useStyles from './styles'
+
+const Paginate = () =>{
+  const classes = useStyles();
+  return(
+    <div>
+      <Pagination 
+      className={{ul : classes.ul}}
+      count={5}
+      page={1}
+      variant="outlined"
+      color="primary"
+      renderItem={(item) => (
+        <PaginationItem {...item} component={Link} to={`/posts?page=${1}`}/>
+      )}
+      />
+    </div>
+  )
+}
+export default Paginate;
