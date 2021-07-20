acf954684c8db3dcb0751f13250f3e502cfb17dd
shikhasingh07 <shikha.thakur2295@gmail.com>
2021-07-20 15:24:57 +0530

    update

--- server/router/posts.js
+++ server/router/posts.js
@@ -7,6 +7,7 @@ import {
   likePost,
   getPost,
   getPostBySearch,
+  commentPost,
 } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 //applying middle ware to know that person is allowed or not
@@ -18,5 +19,6 @@ router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
 router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", auth, commentPost);
 
 export default router;
